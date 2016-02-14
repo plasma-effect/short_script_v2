f74734f5e9347afd21ab59bd2eeb78d09b7330a3
plasma_effect <dtyazsk_windows7@yahoo.co.jp>
2016-02-14 22:05:40 +0900

    [C#]class DefaultFunctionがなんかしらんけどpublicじゃなかったのでpublicにした

---
+++ short_script_v2_csharp/short_script_v2_csharp/DefaultFunction.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortScriptV2
+{
+    public class DefaultFunction
+    {
+        private static dynamic BiOperator(dynamic[] d,CodeData data,Func<dynamic,dynamic,dynamic> op,string opname)
+        {
+            try
+            {
+                return op(d[0], d[1]);
+            }
+            catch(Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(string.Format("'{0}' {1} '{2}' are not allowed", d[0].GetType(), opname, d[1].GetType())));
+            }
+        }
+
+        private static void MakeBiOperator(Dictionary<string, IFunction> dic, string opname, Func<dynamic, dynamic, dynamic> func)
+        {
+            dic.Add(opname, new NonAssistedFunction(opname, (d, data) => (BiOperator(d, data, func, opname)), true, 2));
+        }
+
+        public static Dictionary<string,IFunction> MakeBiOperatorFunctions()
+        {
+            var ret = new Dictionary<string, IFunction>();
+            MakeBiOperator(ret, "+", (a, b) => a + b);
+            MakeBiOperator(ret, "-", (a, b) => a - b);
+            MakeBiOperator(ret, "*", (a, b) => a * b);
+            MakeBiOperator(ret, "/", (a, b) => a / b);
+            MakeBiOperator(ret, "%", (a, b) => a % b);
+
+            MakeBiOperator(ret, "&", (a, b) => a & b);
+            MakeBiOperator(ret, "|", (a, b) => a | b);
+            MakeBiOperator(ret, "^", (a, b) => a ^ b);
+            MakeBiOperator(ret, "<<", (a, b) => a << b);
+            MakeBiOperator(ret, ">>", (a, b) => a >> b);
+            MakeBiOperator(ret, "&&", (a, b) => a && b);
+            MakeBiOperator(ret, "||", (a, b) => a || b);
+
+            MakeBiOperator(ret, "=", (a, b) => a == b);
+            MakeBiOperator(ret, "<>", (a, b) => a != b);
+            MakeBiOperator(ret, "<", (a, b) => a < b);
+            MakeBiOperator(ret, "<=", (a, b) => a <= b);
+            MakeBiOperator(ret, ">", (a, b) => a > b);
+            MakeBiOperator(ret, ">=", (a, b) => a >= b);
+            
+            return ret;
+        }
+
+        private static dynamic Print(dynamic[] d,CodeData data)
+        {
+            Console.Write(d[0]);
+            return d[0];
+        }
+
+        private static dynamic PrintLine(dynamic[] d, CodeData data)
+        {
+            Console.WriteLine(d[0]);
+            return d[0];
+        }
+
+        private static dynamic ArrowAccess(dynamic[] d,CodeData data)
+        {
+            int i;
+            try
+            {
+                i = d[1];   
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(string.Format("Transforming '{0}' to 'integer' is not  allowed.", d[1].GetType())));
+            }
+            try
+            {
+                return d[0][i];
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(string.Format("'{0}' is not array or given argument Length is '0'.", d[0].GetType())));
+            }
+        }
+
+        private static dynamic StringToInt(dynamic[] d, CodeData data)
+        {
+            string s;
+            try
+            {
+                s = d[0];
+            }
+            catch(Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(string.Format("Transforming '{0}' to 'string' is not  allowed.", d[1].GetType())));
+            }
+            try
+            {
+                return int.Parse(s);
+            }
+            catch(Exception)
+            {
+                throw new InnerException(data.ExceptionMessage("Given argument can not be parsed to integer."));
+            }
+        }
+
+        private static dynamic StringToDouble(dynamic[] d, CodeData data)
+        {
+            string s;
+            try
+            {
+                s = d[0];
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(string.Format("Transforming '{0}' to 'string' is not allowed.", d[1].GetType())));
+            }
+            try
+            {
+                return double.Parse(s);
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage("Given argument can not be parsed to double."));
+            }
+        }
+
+        private static dynamic MakeArray(dynamic[] d,CodeData data)
+        {
+            return d;
+        }
+
+        private static Type GetTypeI<T>(T v)
+        {
+            return typeof(T);
+        }
+
+        private static Type GetType(dynamic[] d, CodeData data)
+        {
+            return GetTypeI(d[0]);
+        }
+
+        public static Dictionary<string,IFunction> MakeUtilityFunctions()
+        {
+            var ret = new Dictionary<string, IFunction>();
+            ret.Add("print", new NonAssistedFunction("print", Print, false));
+            ret.Add("println", new NonAssistedFunction("println", PrintLine, false));
+            ret.Add("at", new NonAssistedFunction("at", ArrowAccess, true, 2));
+            ret.Add("int.parse", new NonAssistedFunction("int.parse", StringToInt, true));
+            ret.Add("double.parse", new NonAssistedFunction("double.parse", StringToDouble, true));
+            ret.Add("array", new NonAssistedFunction("array", MakeArray, true, -1));
+            ret.Add("type", new NonAssistedFunction("type", GetType, false));
+            return ret;
+        }
+
+        public static Dictionary<string,IFunction> GetDefaultFunctions()
+        {
+            var ret = MakeBiOperatorFunctions();
+            ret = ret.Concat(MakeUtilityFunctions()).ToDictionary(x => x.Key, x => x.Value);
+            return ret;
+        }
+
+        public static Dictionary<string,SFunction> MakeUtilityCommand()
+        {
+            var ret = new Dictionary<string, SFunction>();
+            return ret;
+        }
+
+        public static Dictionary<string,SFunction> GetDefaultCommand()
+        {
+            var ret = MakeUtilityCommand();
+            return ret;
+        }
+
+        private static dynamic LiteralCheckInteger(string str)
+        {
+            int v;
+            return int.TryParse(str, out v) ? v as dynamic : null as dynamic;
+        }
+
+        private static dynamic LiteralCheckString(string str)
+        {
+            return str[0] == '"' && str[str.Length - 1] == '"' ? str.Substring(1, str.Length - 2) as dynamic : null;
+        }
+
+        private static dynamic LiteralCheckDouble(string str)
+        {
+            double v;
+            return double.TryParse(str, out v) ? v as dynamic : null as dynamic;
+        }
+
+        private static dynamic LiteralCheckBoolean(string str)
+        {
+            return str == "true" ? true as dynamic : str == "false" ? false as dynamic : null;
+        }
+
+        public static List<Func<string,dynamic>> GetDefaultLiteralChecker()
+        {
+            List<Func<string, dynamic>> ret = new List<Func<string, dynamic>>();
+            ret.Add(LiteralCheckInteger);
+            ret.Add(LiteralCheckString);
+            ret.Add(LiteralCheckDouble);
+            ret.Add(LiteralCheckBoolean);
+            return ret;
+        }
+    }
+}
---
+++ short_script_v2_csharp/short_script_v2_csharp/SystemFunction.cs
@@ -0,0 +1,606 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortScriptV2
+{
+    public abstract class SFunction
+    {
+        public abstract void Call(dynamic[] argument, ScriptRunner runner, CodeData data);
+        public abstract int ArgumentLength { get; }
+        public abstract string Name { get;}
+    }
+
+    public class SystemFunction<T0> : SFunction
+    {
+        Action<T0, ScriptRunner> action;
+        string name;
+        string argument_message;
+        string running_message;
+
+        public override int ArgumentLength
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override void Call(dynamic[] argument, ScriptRunner runner, CodeData data)
+        {
+            if (argument.Length != 1
+                || !(argument[0] is T0))
+                throw new InnerException(data.ExceptionMessage(argument_message));
+            try
+            {
+                action(argument[0], runner);
+            }
+            catch(Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(running_message));
+            }
+        }
+
+        public SystemFunction(string name,Action<T0,ScriptRunner> action, string argument_message = "Argument Type Error",string running_message="Running Error")
+        {
+            this.action = action;
+            this.name = name;
+            this.argument_message = argument_message;
+            this.running_message = running_message;
+        }
+    }
+    public class SystemFunction<T0, T1> : SFunction
+    {
+        Action<T0, T1, ScriptRunner> action;
+        string name;
+        string argument_message;
+        string running_message;
+
+        public override int ArgumentLength
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override void Call(dynamic[] argument, ScriptRunner runner, CodeData data)
+        {
+            if (argument.Length != 2
+|| !(argument[0] is T0)
+|| !(argument[1] is T1))
+                throw new InnerException(data.ExceptionMessage(argument_message));
+            try
+            {
+                action(argument[0], argument[1], runner);
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(running_message));
+            }
+        }
+
+        public SystemFunction(string name, Action<T0, T1, ScriptRunner> action, string argument_message = "Argument Type Error", string running_message = "Running Error")
+        {
+            this.action = action;
+            this.name = name;
+            this.argument_message = argument_message;
+            this.running_message = running_message;
+        }
+    }
+    public class SystemFunction<T0, T1, T2> : SFunction
+    {
+        Action<T0, T1, T2, ScriptRunner> action;
+        string name;
+        string argument_message;
+        string running_message;
+
+        public override int ArgumentLength
+        {
+            get
+            {
+                return 3;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override void Call(dynamic[] argument, ScriptRunner runner, CodeData data)
+        {
+            if (argument.Length != 3
+|| !(argument[0] is T0)
+|| !(argument[1] is T1)
+|| !(argument[2] is T2))
+                throw new InnerException(data.ExceptionMessage(argument_message));
+            try
+            {
+                action(argument[0], argument[1], argument[2], runner);
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(running_message));
+            }
+        }
+
+        public SystemFunction(string name, Action<T0, T1, T2, ScriptRunner> action, string argument_message = "Argument Type Error", string running_message = "Running Error")
+        {
+            this.action = action;
+            this.name = name;
+            this.argument_message = argument_message;
+            this.running_message = running_message;
+        }
+    }
+    public class SystemFunction<T0, T1, T2, T3> : SFunction
+    {
+        Action<T0, T1, T2, T3, ScriptRunner> action;
+        string name;
+        string argument_message;
+        string running_message;
+
+        public override int ArgumentLength
+        {
+            get
+            {
+                return 4;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override void Call(dynamic[] argument, ScriptRunner runner, CodeData data)
+        {
+            if (argument.Length != 4
+|| !(argument[0] is T0)
+|| !(argument[1] is T1)
+|| !(argument[2] is T2)
+|| !(argument[3] is T3))
+                throw new InnerException(data.ExceptionMessage(argument_message));
+            try
+            {
+                action(argument[0], argument[1], argument[2], argument[3], runner);
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(running_message));
+            }
+        }
+
+        public SystemFunction(string name, Action<T0, T1, T2, T3, ScriptRunner> action, string argument_message = "Argument Type Error", string running_message = "Running Error")
+        {
+            this.action = action;
+            this.name = name;
+            this.argument_message = argument_message;
+            this.running_message = running_message;
+        }
+    }
+    public class SystemFunction<T0, T1, T2, T3, T4> : SFunction
+    {
+        Action<T0, T1, T2, T3, T4, ScriptRunner> action;
+        string name;
+        string argument_message;
+        string running_message;
+
+        public override int ArgumentLength
+        {
+            get
+            {
+                return 5;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override void Call(dynamic[] argument, ScriptRunner runner, CodeData data)
+        {
+            if (argument.Length != 5
+|| !(argument[0] is T0)
+|| !(argument[1] is T1)
+|| !(argument[2] is T2)
+|| !(argument[3] is T3)
+|| !(argument[4] is T4))
+                throw new InnerException(data.ExceptionMessage(argument_message));
+            try
+            {
+                action(argument[0], argument[1], argument[2], argument[3], argument[4], runner);
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(running_message));
+            }
+        }
+
+        public SystemFunction(string name, Action<T0, T1, T2, T3, T4, ScriptRunner> action, string argument_message = "Argument Type Error", string running_message = "Running Error")
+        {
+            this.action = action;
+            this.name = name;
+            this.argument_message = argument_message;
+            this.running_message = running_message;
+        }
+    }
+    public class SystemFunction<T0, T1, T2, T3, T4, T5> : SFunction
+    {
+        Action<T0, T1, T2, T3, T4, T5, ScriptRunner> action;
+        string name;
+        string argument_message;
+        string running_message;
+
+        public override int ArgumentLength
+        {
+            get
+            {
+                return 6;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override void Call(dynamic[] argument, ScriptRunner runner, CodeData data)
+        {
+            if (argument.Length != 6
+|| !(argument[0] is T0)
+|| !(argument[1] is T1)
+|| !(argument[2] is T2)
+|| !(argument[3] is T3)
+|| !(argument[4] is T4)
+|| !(argument[5] is T5))
+                throw new InnerException(data.ExceptionMessage(argument_message));
+            try
+            {
+                action(argument[0], argument[1], argument[2], argument[3], argument[4], argument[5], runner);
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(running_message));
+            }
+        }
+
+        public SystemFunction(string name, Action<T0, T1, T2, T3, T4, T5, ScriptRunner> action, string argument_message = "Argument Type Error", string running_message = "Running Error")
+        {
+            this.action = action;
+            this.name = name;
+            this.argument_message = argument_message;
+            this.running_message = running_message;
+        }
+    }
+    public class SystemFunction<T0, T1, T2, T3, T4, T5, T6> : SFunction
+    {
+        Action<T0, T1, T2, T3, T4, T5, T6, ScriptRunner> action;
+        string name;
+        string argument_message;
+        string running_message;
+
+        public override int ArgumentLength
+        {
+            get
+            {
+                return 7;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override void Call(dynamic[] argument, ScriptRunner runner, CodeData data)
+        {
+            if (argument.Length != 7
+|| !(argument[0] is T0)
+|| !(argument[1] is T1)
+|| !(argument[2] is T2)
+|| !(argument[3] is T3)
+|| !(argument[4] is T4)
+|| !(argument[5] is T5)
+|| !(argument[6] is T6))
+                throw new InnerException(data.ExceptionMessage(argument_message));
+            try
+            {
+                action(argument[0], argument[1], argument[2], argument[3], argument[4], argument[5], argument[6], runner);
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(running_message));
+            }
+        }
+
+        public SystemFunction(string name, Action<T0, T1, T2, T3, T4, T5, T6, ScriptRunner> action, string argument_message = "Argument Type Error", string running_message = "Running Error")
+        {
+            this.action = action;
+            this.name = name;
+            this.argument_message = argument_message;
+            this.running_message = running_message;
+        }
+    }
+    public class SystemFunction<T0, T1, T2, T3, T4, T5, T6, T7> : SFunction
+    {
+        Action<T0, T1, T2, T3, T4, T5, T6, T7, ScriptRunner> action;
+        string name;
+        string argument_message;
+        string running_message;
+
+        public override int ArgumentLength
+        {
+            get
+            {
+                return 8;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override void Call(dynamic[] argument, ScriptRunner runner, CodeData data)
+        {
+            if (argument.Length != 8
+|| !(argument[0] is T0)
+|| !(argument[1] is T1)
+|| !(argument[2] is T2)
+|| !(argument[3] is T3)
+|| !(argument[4] is T4)
+|| !(argument[5] is T5)
+|| !(argument[6] is T6)
+|| !(argument[7] is T7))
+                throw new InnerException(data.ExceptionMessage(argument_message));
+            try
+            {
+                action(argument[0], argument[1], argument[2], argument[3], argument[4], argument[5], argument[6], argument[7], runner);
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(running_message));
+            }
+        }
+
+        public SystemFunction(string name, Action<T0, T1, T2, T3, T4, T5, T6, T7, ScriptRunner> action, string argument_message = "Argument Type Error", string running_message = "Running Error")
+        {
+            this.action = action;
+            this.name = name;
+            this.argument_message = argument_message;
+            this.running_message = running_message;
+        }
+    }
+    public class SystemFunction<T0, T1, T2, T3, T4, T5, T6, T7, T8> : SFunction
+    {
+        Action<T0, T1, T2, T3, T4, T5, T6, T7, T8, ScriptRunner> action;
+        string name;
+        string argument_message;
+        string running_message;
+
+        public override int ArgumentLength
+        {
+            get
+            {
+                return 9;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override void Call(dynamic[] argument, ScriptRunner runner, CodeData data)
+        {
+            if (argument.Length != 9
+|| !(argument[0] is T0)
+|| !(argument[1] is T1)
+|| !(argument[2] is T2)
+|| !(argument[3] is T3)
+|| !(argument[4] is T4)
+|| !(argument[5] is T5)
+|| !(argument[6] is T6)
+|| !(argument[7] is T7)
+|| !(argument[8] is T8))
+                throw new InnerException(data.ExceptionMessage(argument_message));
+            try
+            {
+                action(argument[0], argument[1], argument[2], argument[3], argument[4], argument[5], argument[6], argument[7], argument[8], runner);
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(running_message));
+            }
+        }
+
+        public SystemFunction(string name, Action<T0, T1, T2, T3, T4, T5, T6, T7, T8, ScriptRunner> action, string argument_message = "Argument Type Error", string running_message = "Running Error")
+        {
+            this.action = action;
+            this.name = name;
+            this.argument_message = argument_message;
+            this.running_message = running_message;
+        }
+    }
+    public class SystemFunction<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9> : SFunction
+    {
+        Action<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, ScriptRunner> action;
+        string name;
+        string argument_message;
+        string running_message;
+
+        public override int ArgumentLength
+        {
+            get
+            {
+                return 10;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override void Call(dynamic[] argument, ScriptRunner runner, CodeData data)
+        {
+            if (argument.Length != 10
+|| !(argument[0] is T0)
+|| !(argument[1] is T1)
+|| !(argument[2] is T2)
+|| !(argument[3] is T3)
+|| !(argument[4] is T4)
+|| !(argument[5] is T5)
+|| !(argument[6] is T6)
+|| !(argument[7] is T7)
+|| !(argument[8] is T8)
+|| !(argument[9] is T9))
+                throw new InnerException(data.ExceptionMessage(argument_message));
+            try
+            {
+                action(argument[0], argument[1], argument[2], argument[3], argument[4], argument[5], argument[6], argument[7], argument[8], argument[9], runner);
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(running_message));
+            }
+        }
+
+        public SystemFunction(string name, Action<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, ScriptRunner> action, string argument_message = "Argument Type Error", string running_message = "Running Error")
+        {
+            this.action = action;
+            this.name = name;
+            this.argument_message = argument_message;
+            this.running_message = running_message;
+        }
+    }
+    public class VariadicSystemFunction<T> : SFunction
+    {
+        Action<T[], ScriptRunner> action;
+        string name;
+        string argument_message;
+        string running_message;
+
+        public override int ArgumentLength
+        {
+            get
+            {
+                return -1;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override void Call(dynamic[] argument, ScriptRunner runner, CodeData data)
+        {
+            var a = new T[argument.Length];
+            for(int i=0;i<argument.Length;++i)
+            {
+                if (!(argument[i] is T))
+                    throw new InnerException(data.ExceptionMessage(argument_message));
+                a[i] = argument[i];
+            }
+            try
+            {
+                action(a, runner);
+            }
+            catch (Exception)
+            {
+                throw new InnerException(data.ExceptionMessage(running_message));
+            }
+        }
+
+        public VariadicSystemFunction(string name, Action<T[], ScriptRunner> action, string argument_message = "Argument Type Error", string running_message = "Running Error")
+        {
+            this.action = action;
+            this.name = name;
+            this.argument_message = argument_message;
+            this.running_message = running_message;
+        }
+    }
+
+    public class NonAssistedSystemFunction : SFunction
+    {
+        Action<dynamic[], ScriptRunner, CodeData> action;
+        string name;
+        int argument_length;
+
+        public override int ArgumentLength
+        {
+            get
+            {
+                return argument_length;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public override void Call(dynamic[] argument, ScriptRunner runner, CodeData data)
+        {
+            action(argument, runner, data);
+        }
+
+        public NonAssistedSystemFunction(string name, Action<dynamic[], ScriptRunner, CodeData> action, int argument_length)
+        {
+            this.action = action;
+            this.name = name;
+            this.argument_length = argument_length;
+        }
+    }
+}
